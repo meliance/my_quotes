86c8bc93362296d531fe8e72c235ad5ed3f2f3c2
meliance <melakmu66@gmail.com>
2024-02-21 10:06:33 +0300

    quote app

---
+++ lib/main.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+import 'quote.dart';
+import 'quote_card.dart';
+
+void main() => runApp(MaterialApp(
+  home: QuoteList(),
+));
+class QuoteList extends StatefulWidget {
+  const QuoteList({Key? key}) : super(key: key);
+
+  @override
+  State<QuoteList> createState() => _QuoteListState();
+}
+
+class _QuoteListState extends State<QuoteList> {
+  List<Quote> quotes = [
+    Quote(author: 'Franklin D. Roosevelt', text: 'The only thing we have to fear is fear itself.'),
+    Quote(author: 'Eleanor Roosevelt', text: 'Do one thing every day that scares you.'),
+    Quote(author: 'Tony Robbins', text: 'The only impossible journey is the one you never begin.'),
+    Quote(author: 'Abraham Lincoln', text: "In the end, it's not the years in your life that count. It's the life in your years."),
+  ];
+
+  void addQuote(String quoteText, String authorText) {
+    setState(() {
+      quotes.add(Quote(author: authorText, text: quoteText));
+    });
+  }
+
+  void deleteQuote(int index) {
+    setState(() {
+      quotes.removeAt(index);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        title: Text('Awesome Quotes'),
+        centerTitle: true,
+        backgroundColor: Colors.indigo[100],
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: quotes.length,
+              itemBuilder: (context, index) {
+                return QuoteCard(
+                  quote: quotes[index],
+                  delete: () => deleteQuote(index),
+                  add: addQuote,
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+
+
---
+++ lib/quote_card.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+import 'quote.dart';
+
+class QuoteCard extends StatelessWidget {
+final Quote quote;
+final void Function()? delete;
+final void Function(String quote, String author)? add;
+
+QuoteCard({required this.quote, required this.delete, required this.add});
+
+final TextEditingController quoteController = TextEditingController();
+final TextEditingController authorController = TextEditingController();
+
+@override
+Widget build(BuildContext context) {
+return Card(
+  margin: EdgeInsets.fromLTRB(16, 16, 16, 0),
+  child: Padding(
+    padding: const EdgeInsets.all(8.0),
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          quote.text,
+          style: TextStyle(
+            fontSize: 18,
+            color: Colors.grey[600],
+          ),
+        ),
+        SizedBox(height: 4),
+        Text(
+          quote.author,
+          style: TextStyle(
+            fontSize: 18,
+            color: Colors.grey[800],
+          ),
+        ),
+        SizedBox(height: 4),
+        if (delete != null)
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              SizedBox(
+                width: 40,
+                height: 40,
+                child: FloatingActionButton(
+                  onPressed: delete,
+                  tooltip: 'Delete quote',
+                  child: Icon(Icons.delete),
+                ),
+              ),
+
+              SizedBox(width: 28),
+              FloatingActionButton(
+                onPressed: () {
+                  showDialog(
+                    context: context,
+                    builder: (context) => AlertDialog(
+                      title: Text('Add New Quote'),
+                      content: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          TextField(
+                            controller: quoteController,
+                            decoration: InputDecoration(labelText: 'Write your Quote'),
+                          ),
+                          TextField(
+                            controller: authorController,
+                            decoration: InputDecoration(labelText: 'Name of Author'),
+                          ),
+                        ],
+                      ),
+                      actions: [
+                        ElevatedButton(
+                          onPressed: () {
+                            String quote = quoteController.text;
+                            String author = authorController.text;
+                            add?.call(quote, author);
+                            quoteController.clear();
+                            authorController.clear();
+                            Navigator.pop(context);
+                          },
+                          child: Text('Add'),
+                        ),
+                      ],
+                    ),
+                  );
+                },
+                tooltip: 'Add quote',
+                mini: true,
+                child: Icon(Icons.add),
+              ),
+            ],
+          ),
+      ],
+    ),
+  ),
+);
+}
+}
